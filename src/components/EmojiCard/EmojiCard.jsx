9dd22ef6d58fc9a69913712244793b55a82b74c1
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-10-06 01:42:28 +0530

    finished EmojiCard layout

--- src/components/EmojiCard/EmojiCard.jsx
+++ src/components/EmojiCard/EmojiCard.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
-import { Paper } from "@mui/material";
+import { Card, CardHeader, IconButton } from "@mui/material";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+
+import "./styles.css";
 
 const EmojiCard = (props) => {
-  const { char, name, category } = props;
-  return <Paper sx={{ m: 3 }}>{char}</Paper>;
+  const [shadow, setShadow] = React.useState(1);
+
+  const onMouseOver = () => setShadow(3);
+  const onMouseOut = () => setShadow(1);
+  const { char, name, group } = props;
+
+  return (
+    <Card
+      sx={{ m: 1 }}
+      elevation={shadow}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    >
+      <CardHeader
+        avatar={<div className="emoji-container">{char}</div>}
+        action={
+          <IconButton>
+            <FileCopyIcon />
+          </IconButton>
+        }
+        title={name}
+        subheader={group}
+      />
+    </Card>
+  );
 };
 
 export default EmojiCard;
--- src/components/EmojiList/EmojiList.jsx
+++ src/components/EmojiList/EmojiList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import emoji from "emoji.json";
 
+import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 
 import EmojiCard from "../EmojiCard/EmojiCard";
 
 const list = [];
-for (let i = 0; i < 10; ++i) {
+for (let i = 0; i < 1000; i += 20) {
   list.push(emoji[i]);
 }
 
@@ -15,14 +16,25 @@ const EmojiList = () => {
     <Box
       pt={4}
       pl={4}
-      style={{
+      pr={4}
+      sx={{
         height: "100vh",
         overflowY: "auto",
+        bgcolor: "#e3f2fd",
       }}
     >
-      {list.map((emoji) => (
-        <EmojiCard key={emoji.codes} {...emoji} />
-      ))}
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        {list.map((emoji) => (
+          <Grid item key={emoji.codes} xs={12} sm={6} md={4} lg={3}>
+            <EmojiCard {...emoji} />
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
